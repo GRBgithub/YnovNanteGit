be049f3de291ccb09d4ccc0107bab91b38e8debf
GRB.github <76528190+GRBgithub@users.noreply.github.com>
2022-11-25 11:44:23 +0100

    Init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Asval from './components/Asval/Asval';
+
+
+function App() {
+  return (
+    <div className="App" id="">
+      <Asval></Asval>
+    </div>
+  );
+}
+
+
+ export default App;
---
+++ src/components/Asval/Asval.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Asval.css"
+
+const Asval = () => {
+    return (
+        <button>
+           ASVAL 
+        </button>
+    );
+};
+
+export default Asval;
+
